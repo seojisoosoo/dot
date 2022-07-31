22a100fe236a609605359cff677d63c9f643abc3
Seojisoo20191941 <amy0480@naver.com>
2022-07-31 22:32:46 +0900

    feat:onclick event

---
+++ dot.js
@@ -0,0 +1,50 @@
+function click1() {
+  const dot1 = document.querySelector("#dot1");
+  const currentColor = dot1.style.backgroundColor;
+  let newColor;
+  if (currentColor != "black") {
+    newColor = "black";
+  }
+  dot1.style.backgroundColor = newColor;
+}
+
+function click2() {
+  const dot2 = document.querySelector("#dot2");
+  const currentColor = dot2.style.backgroundColor;
+  let newColor;
+  if (currentColor != "black") {
+    newColor = "black";
+  }
+  dot2.style.backgroundColor = newColor;
+}
+function click3() {
+  const dot3 = document.querySelector("#dot3");
+  const currentColor = dot3.style.backgroundColor;
+  let newColor;
+  if (currentColor != "black") {
+    newColor = "black";
+  }
+  dot3.style.backgroundColor = newColor;
+}
+function click4() {
+  const dot4 = document.querySelector("#dot4");
+  const currentColor = dot4.style.backgroundColor;
+  let newColor;
+  if (currentColor != "black") {
+    newColor = "black";
+  }
+  dot4.style.backgroundColor = newColor;
+}
+function click5() {
+  const dot5 = document.querySelector("#dot5");
+  const currentColor = dot5.style.backgroundColor;
+  let newColor;
+  if (currentColor != "black") {
+    newColor = "black";
+  }
+  dot5.style.backgroundColor = newColor;
+}
+function oneTwo() {}
+function twoThree() {}
+function threeFour() {}
+function fourFive() {}
