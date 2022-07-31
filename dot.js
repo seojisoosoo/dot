dabbddab3cc5965222e5e2c5a2a414b4ec451b87
Seojisoo20191941 <amy0480@naver.com>
2022-08-01 00:59:40 +0900

    trying line

--- dot.js
+++ dot.js
@@ -2,6 +2,7 @@
 //선 -> 점 검은색으로 변하자마자 나오게
 //일기 확인버튼을 누르면 효과나오기(일기장 페이지에서 연결)->그럴려면 한 페이지에서 해야할 것 같기도...
 //점이 회색이면 일기 쓰는 페이지로 연결, 검은색이면 작성한 페이지로 연결
+
 function click1() {
   const dot1 = document.querySelector("#dot1");
   const currentColor = dot1.style.backgroundColor;
@@ -21,6 +22,15 @@ function click2() {
   if (currentColor != "black") {
     newColor = "black";
     //선
+    const canvas = document.querySelector("#onetwo");
+    var context = canvas.getContext("2d");
+    context.strokeStyle = "black";
+    context.lineWidth = 3;
+
+    context.beginPath();
+    context.moveTo(20, 100);
+    context.lineTo(270, 115);
+    context.stroke();
   } else {
     //다음페이지 나오게 -> html에 연결해두고 display:none해놨다가 display:block으로 변경하자
   }
