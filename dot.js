205b35b340a0e16711b61e65ab436e27f12dd96f
Seojisoo20191941 <amy0480@naver.com>
2022-07-31 23:19:44 +0900

    dot fixing

--- dot.js
+++ dot.js
@@ -1,9 +1,15 @@
+//클릭하는 순간 검은색으로 변경
+//선 -> 점 검은색으로 변하자마자 나오게
+//일기 확인버튼을 누르면 효과나오기(일기장 페이지에서 연결)->그럴려면 한 페이지에서 해야할 것 같기도...
+//점이 회색이면 일기 쓰는 페이지로 연결, 검은색이면 작성한 페이지로 연결
 function click1() {
   const dot1 = document.querySelector("#dot1");
   const currentColor = dot1.style.backgroundColor;
   let newColor;
   if (currentColor != "black") {
     newColor = "black";
+  } else {
+    //다음페이지 나오게 -> html에 연결해두고 display:none해놨다가 display:block으로 변경하자
   }
   dot1.style.backgroundColor = newColor;
 }
@@ -14,6 +20,8 @@ function click2() {
   let newColor;
   if (currentColor != "black") {
     newColor = "black";
+  } else {
+    //다음페이지 나오게 -> html에 연결해두고 display:none해놨다가 display:block으로 변경하자
   }
   dot2.style.backgroundColor = newColor;
 }
@@ -23,6 +31,8 @@ function click3() {
   let newColor;
   if (currentColor != "black") {
     newColor = "black";
+  } else {
+    //다음페이지 나오게 -> html에 연결해두고 display:none해놨다가 display:block으로 변경하자
   }
   dot3.style.backgroundColor = newColor;
 }
@@ -32,6 +42,8 @@ function click4() {
   let newColor;
   if (currentColor != "black") {
     newColor = "black";
+  } else {
+    //다음페이지 나오게 -> html에 연결해두고 display:none해놨다가 display:block으로 변경하자
   }
   dot4.style.backgroundColor = newColor;
 }
@@ -41,10 +53,8 @@ function click5() {
   let newColor;
   if (currentColor != "black") {
     newColor = "black";
+  } else {
+    //다음페이지 나오게 -> html에 연결해두고 display:none해놨다가 display:block으로 변경하자
   }
   dot5.style.backgroundColor = newColor;
 }
-function oneTwo() {}
-function twoThree() {}
-function threeFour() {}
-function fourFive() {}
